d3947fe3fb9b7c906bcea2da68cb33704c9eeaa7
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/Class1.cs
@@ -0,0 +1,35 @@
+namespace BindWebReportToArrayList {
+    public class Record {
+        int id, age;
+        string name;
+        public Record(int id, string name, int age) {
+            this.id = id;
+            this.name = name;
+            this.age = age;
+        }
+        public int ID {
+            get {
+                return id;
+            }
+            set {
+                id = value;
+            }
+        }
+        public string Name {
+            get {
+                return name;
+            }
+            set {
+                name = value;
+            }
+        }
+        public int Age {
+            get {
+                return age;
+            }
+            set {
+                age = value;
+            }
+        }
+    }
+}
